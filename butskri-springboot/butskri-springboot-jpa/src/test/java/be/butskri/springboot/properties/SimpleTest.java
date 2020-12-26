7e9da244c07e855c224ca0186364f79052b29ed0
butskri <kristof.buts@gmail.com>
2020-12-26 10:39:30 +0100

    springboot-jpa setup

---
+++ butskri-springboot/butskri-springboot-jpa/src/test/java/be/butskri/springboot/properties/SimpleTest.java
@@ -0,0 +1,13 @@
+package be.butskri.springboot.properties;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// TODO remove this class later again
+public class SimpleTest {
+
+    @Test
+    public void dd() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
